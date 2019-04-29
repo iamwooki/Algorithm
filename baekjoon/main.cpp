fcdbab846be7bf81642988582dda2632db4d9cf6
HyunWook, Hong <hwhong.korea@gmail.com>
2019-04-29 21:42:34 +0900

    Initial Commit

---
+++ baekjoon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  baekjoon
+//
+//  Created by HyunWook Hong on 29/04/2019.
+//  Copyright © 2019 HyunWook Hong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
