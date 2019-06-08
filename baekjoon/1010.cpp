96a4a2d2dda6a37c863e147c281439357f739c21
HyunWook, Hong <hwhong.korea@gmail.com>
2019-06-08 20:15:58 +0900

    build bridges using to DF ( combination)

--- baekjoon/1010.cpp
+++ baekjoon/1010.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 int d[31][31]; // combination
 int foo(int n, int r) {
-    if (n == r || r == 0)
-        return 1;
-    if (d[n][r]) return d[n][r];
+    if (n == r || r == 0) return 1; //조합조건에 의해 
+    if (d[n][r]) return d[n][r]; //d[n][r]번째에 값이 있다면 (재귀가 모두 끝난 경우)
     return d[n][r] = foo(n - 1, r - 1) + foo(n - 1, r); // nCr = n-1Cr-1 + n-1Cr
 }
 int main() {
