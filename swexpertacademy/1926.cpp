886c4f03e938d7cac44e0d8f2766b2a43166cdd8
HyunWook, Hong <hwhong.korea@gmail.com>
2019-09-23 21:16:49 +0900

    3

---
+++ swexpertacademy/1926.cpp
@@ -0,0 +1,31 @@
+//
+//  1926.cpp
+//  baekjoon
+//
+//  Created by HyunWook Hong on 23/09/2019.
+//  Copyright © 2019 HyunWook Hong. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N; cin>>N;
+    for(int i=1;i<=N;++i){
+        int number = i;
+        int cnt = 0;
+        while(number){
+            if( number % 10 == 3 || number % 10 == 6 || number % 10 == 9) ++cnt;
+            number /= 10;
+        }
+        if(!cnt){
+            cout<<i<<" ";
+            continue;
+        }
+        while(cnt--){
+            cout<<"-";
+        }
+        cout<<" ";
+    }
+    return 0;
+}
